60a7c908291125eed168a2666c96d4346e2335fe
asgharas <asgharalishah2001@gmail.com>
2023-05-25 19:21:11 +0500

    completed

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 9,
+    text: 'lorem ipsum dolor sit amet consectetur adipisicing elit. this is just anoter feedback',
+  },
+  {
+    id: 2,
+    rating: 4,
+    text: 'lorem ipsum dolor sit amet consectetur adipisicing elit. this is just anoter feedback',
+  },
+  {
+    id: 3,
+    rating: 6,
+    text: 'lorem ipsum dolor sit amet consectetur adipisicing elit. this is just anoter feedback',
+  },
+]
+
+export default FeedbackData
